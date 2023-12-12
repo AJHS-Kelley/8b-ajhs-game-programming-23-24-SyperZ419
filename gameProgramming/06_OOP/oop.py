c074624cae2e9d5181cc69ae2dab6ee4e4638456
SyperZ419 <s11328481@students.duvalschools.org>
2023-12-12 13:52:15 -0500

    Update oop.py
    created class Person, and printed the person1 variable to the screeen.

--- gameProgramming/06_OOP/oop.py
+++ gameProgramming/06_OOP/oop.py
@@ -1,2 +1,14 @@
 # Object-Oriented Programming, Xavier Oliver, v0.0
 class Person: # Use PascalCase for ClassNames
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+    
+    # To string function --> how object appears as a string
+    def __str__(self):
+        return f"Name: {self.name}\nThis person is {self.age} years old.\nThey weigh {self.weight} pounds."
+
+
+person1 = Person("Guts", 24, 254)
+print(person1)
