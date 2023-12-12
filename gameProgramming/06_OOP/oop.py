4a8657d5396f0d2067b4cf875d6b566a61aae683
SyperZ419 <s11328481@students.duvalschools.org>
2023-12-12 14:05:16 -0500

    Update oop.py
    made if else statements using person1 and 2

--- gameProgramming/06_OOP/oop.py
+++ gameProgramming/06_OOP/oop.py
@@ -1,4 +1,4 @@
-# Object-Oriented Programming, Xavier Oliver, v0.1
+# Object-Oriented Programming, Xavier Oliver, v0.2
 class Person: # Use PascalCase for ClassNames
     def __init__(self, name, age, weight):
         self.name = name
@@ -12,3 +12,20 @@ class Person: # Use PascalCase for ClassNames
 
 person1 = Person("Guts", 24, 254)
 print(person1)
+
+person2 = Person("Naoki Kashima", 18, 185.5)
+print(person2)
+
+if person1.weight > person2.weight:
+    print(f"{person1.name} weighs more than {person2.name}.")
+elif person1.weight == person2.weight:
+    print(f"{person1.name} weighs the same as {person2.name}.")
+else:
+    print(f"{person1.name} weighs less than {person2.name}.")
+
+if person1.age > person2.age:
+    print(f"{person1.name} is older than {person2.name}.")
+elif person1.age == person2.age:
+    print(f"{person1.name} is the same age as {person2.name}.")
+else:
+    print(f"{person1.name} is younger than {person2.name}.")
