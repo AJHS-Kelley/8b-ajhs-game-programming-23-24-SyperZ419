456dbd91fd12ca0ca16c6a1390e0ea2a684fa1ac
SyperZ419 <s11328481@students.duvalschools.org>
2023-12-14 13:36:07 -0500

    Create dice.py

---
+++ gameProgramming/Modules/dice.py
@@ -0,0 +1,12 @@
+# Dice Rolling Module, Xavier Oliver, v0.0
+import random
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"Sum: {sum}\n")
+        numRolled += 1
+    return sum
