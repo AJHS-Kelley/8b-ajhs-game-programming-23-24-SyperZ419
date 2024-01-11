c231215d37ad85c553ba2ff265d45b2cd2aea546
SyperZ419 <s11328481@students.duvalschools.org>
2024-01-11 14:24:54 -0500

    Update dnaGame.py

--- gameProgramming/cohort_lessons/dnaGame/dnaGame.py
+++ gameProgramming/cohort_lessons/dnaGame/dnaGame.py
@@ -1,7 +1,28 @@
-# DNA Replication Game, Ryan Kelley, v0.0
+# DNA Replication Game, Ryan Kelley, v0.1
 
 # Import Entire Modules -- Get the whole toolbox.
 import time, datetime
 
 # Import Specific Methods -- Get the specific tool.
 from random import choice
+
+# Store the DNA Bases
+dnaBases = ["G", "A", "C", "T"]
+
+# Game Functions
+def gameIntro() -> None:
+    pass
+
+def genDNA() -> str:
+    basesGenerated = 0
+    basesRequested = int(input("Please enter a positive integer for the number of bases to create.\n"))
+    dnaSequence = ""
+    
+    while basesGenerated < basesRequested:
+        dnaSequence += choice(dnaBases)
+        basesGenerated += 1
+    
+    return dnaSequence
+
+dna = genDNA()
+print(dna)
