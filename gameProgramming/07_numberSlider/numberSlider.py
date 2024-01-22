b480a02a5a4ea8ce35d27bea440c04f6ac429ced
SyperZ419 <s11328481@students.duvalschools.org>
2024-01-22 13:51:16 -0500

    Update numberSlider.py

--- gameProgramming/07_numberSlider/numberSlider.py
+++ gameProgramming/07_numberSlider/numberSlider.py
@@ -1,1 +1,7 @@
 # Number Slider, Xavier Oliver, based on a project by Al Sweigart, v0.0 
+
+import sys, random, pygame
+# sys module provides access to system resources (i.e. Operating System Commands)
+
+from pygame.locals import *
+# Allows us to call functions from pygame using just the function name instead of module.function()
