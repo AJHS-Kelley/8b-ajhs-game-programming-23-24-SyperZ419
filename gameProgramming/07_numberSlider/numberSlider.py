20eb63052d10e8c943b49266996708bb58810fd8
SyperZ419 <s11328481@students.duvalschools.org>
2024-01-22 14:03:55 -0500

    Update numberSlider.py

--- gameProgramming/07_numberSlider/numberSlider.py
+++ gameProgramming/07_numberSlider/numberSlider.py
@@ -12,5 +12,14 @@ BOARDHEIGHT = 4 # Rows
 TILESIZE = 80 # Measured in Pixels
 WINDOWWIDTH = 640 # Pixels
 WINDOWHEIGHT = 480 # Pixels
-FPS = 30
+FPS = 30 # This is a maximum value! Won't make a slow computer run faster.
 BLANK = None
+
+# Color Values in (R, G, B) format
+# 0 = No Value, 255 = Max Value
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BRIGHTBLUE = (0, 50, 255)
+DARKTURQUOISE = (3, 54, 73)
+GREEN = (0, 204, 0)
+
