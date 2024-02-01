4aa84e5e719cdd050b0e489d19db425e89a068d7
SyperZ419 <s11328481@students.duvalschools.org>
2024-02-01 14:18:47 -0500

    Completed game screen

--- gameProgramming/08_ultimatePygame/displayingImages.py
+++ gameProgramming/08_ultimatePygame/displayingImages.py
@@ -4,9 +4,11 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
+testFont = pygame.font.Font(None, 50)
 
-skySurface = pygame.image.load("img/ultimatePygame/Sky.png")
-groundSurface = pygame.image.load("img/ultimatePygame/Ground.png")
+skySurface = pygame.image.load("img/ultimatePygame/Sky.jpg")
+groundSurface = pygame.image.load("img/ultimatePygame/Ground.jpg")
+textSurface = testFont.render("My game", False, "Green")
 
 while True:
     for event in pygame.event.get():
@@ -14,8 +16,10 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(skySurface,(0, 0))
-    screen.blit(groundSurface,(0, 0))
-
+    
+    screen.blit(skySurface,(0,0))
+    screen.blit(groundSurface,(0,300))
+    screen.blit(textSurface,(300,50))
+    
     pygame.display.update()
     clock.tick(60)
