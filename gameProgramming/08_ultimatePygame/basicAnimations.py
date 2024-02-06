f30c7f5366816814e465d7c646eb28fafb73cc96
SyperZ419 <s11328481@students.duvalschools.org>
2024-02-06 12:53:26 -0500

    game files

---
+++ gameProgramming/08_ultimatePygame/basicAnimations.py
@@ -0,0 +1,28 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Runner")
+clock = pygame.time.Clock()
+testFont = pygame.font.Font(None, 50)
+
+skySurface = pygame.image.load("img/ultimatePygame/Sky.jpg")
+groundSurface = pygame.image.load("img/ultimatePygame/Ground.jpg")
+textSurface = testFont.render("My game", False, "Black")
+
+snailSurface = pygame.image.load("img/ultimatePygame/Snail.jpg")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    
+    screen.blit(skySurface,(0,0))
+    screen.blit(groundSurface,(0,300))
+    screen.blit(textSurface,(300,50))
+    screen.blit(snailSurface,(600,250))
+    
+    pygame.display.update()
+    clock.tick(60)
