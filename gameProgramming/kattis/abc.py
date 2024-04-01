b4eb25d68b89100e2126217b33de8720ddae5511
SyperZ419 <s11328481@students.duvalschools.org>
2024-04-01 14:13:19 -0400

    Update abc.py

--- gameProgramming/kattis/abc.py
+++ gameProgramming/kattis/abc.py
@@ -1,16 +1,10 @@
-# input the integers
-# .split() the 3 integers
-# cast the 3 variables to integers
-# assign correct values from least to greatest
-# A < B < C
-
 numbers = input()
 a, b, c = numbers.split()
 a = int(a)
 b = int(b)
 c = int(c)
 
-print(f"a: {a}, b: {b}, c: {c}")
+#print(f"a: {a}, b: {b}, c: {c}")
 if a >= b:
     a, b = b, a
 
@@ -18,4 +12,22 @@ if b >= c:
     b, c = c, b
 if b <= a:
     a, b = b, c
-print(f"a: {a}, b: {b}, c: {c}")
+#print(f"a: {a}, b: {b}, c: {c}")
+
+# input string variable
+# determine order of A, B, C
+# create correct string
+# output string
+
+order = input()
+string = ""
+
+for i in range(len(order)):
+    if order[i] == "A":
+        string += str(a) + " "
+    elif order[i] == "B":
+        string += str(b) + " "
+    else:
+        string += str(c) + " "
+
+print(string)
